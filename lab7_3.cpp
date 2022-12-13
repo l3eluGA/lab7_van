fc1308d292ba7154b1dd28d858071c5a9f86eac8
l3eluGA <rungthiporange@gmail.com>
2022-12-13 16:26:55 +0700

    complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,34 @@
 #include<iostream>
 
 using namespace std;
-
+int adiff(int a,int b)
+{
+  int c;
+    if(b>a)
+    {
+      c=b-a;
+    }
+    else
+    {
+      c=a-b;
+    }
+    if(c%360 >180)
+    {
+      return 360-c%360;
+    }
+    else
+    {
+      return c%360;
+    }
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270)<<"\n";
+  cout << adiff(210,45)<<"\n";
+  cout << adiff(0,360)<<"\n";
+  cout << adiff(10,350)<<"\n";
+  cout << adiff(95,260)<<"\n";
+  cout << adiff(90,-90)<<"\n";
+  cout << adiff(1000,280)<<"\n";
+  cout << adiff(60,244)<<"\n";
 }
